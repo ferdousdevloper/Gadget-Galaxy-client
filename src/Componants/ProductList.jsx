97d3eb77ad17d9bd8238ed45683d8c6024c0adbe
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-08-16 22:08:48 +0600

    sort function added

--- src/Componants/ProductList.jsx
+++ src/Componants/ProductList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './styles.css'; // Import your custom CSS file
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
@@ -7,7 +8,7 @@ const ProductList = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [filters, setFilters] = useState({ brand: '', category: '', priceRange: '' });
     const [sort, setSort] = useState('');
-    const [selectedProduct, setSelectedProduct] = useState(null); // State to store selected product for the modal
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
     useEffect(() => {
         fetchProducts();
@@ -16,12 +17,12 @@ const ProductList = () => {
     const fetchProducts = async () => {
         try {
             let query = `?page=${page}&limit=9${searchQuery ? `&search=${searchQuery}` : ''}`;
-    
+
             if (filters.brand) query += `&brand=${filters.brand}`;
             if (filters.category) query += `&category=${filters.category}`;
             if (filters.priceRange) query += `&priceRange=${filters.priceRange}`;
             if (sort) query += `&sort=${sort}`;
-    
+
             const response = await fetch(`http://localhost:5000/products${query}`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -56,39 +57,38 @@ const ProductList = () => {
     };
 
     const handleProductClick = (product) => {
-        setSelectedProduct(product); // Set the selected product for the modal
+        setSelectedProduct(product);
     };
 
     const handleCloseModal = () => {
-        setSelectedProduct(null); // Close the modal
+        setSelectedProduct(null);
     };
 
     return (
-        <div className="container mx-auto p-6">
-            <h1 className="text-4xl font-bold text-center mb-10">Gadget Galaxy Products</h1>
-            {/* Filters and Sorting */}
+        <div className="container mx-auto p-6 ">
+            <h1 className="text-4xl font-bold text-center mb-10 text-white">Gadget Galaxy Products</h1>
+
             <div className="grid grid-cols-1 md:grid-cols-7 gap-4 my-10">
-                {/* Search */}
-            <div className="col-span-3 flex space-x-4">
-                <input 
-                    type="text" 
-                    placeholder="Search by product model..." 
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button 
-                    onClick={handleSearchClick}
-                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                >
-                    Search
-                </button>
-            </div>
-                {/* Filter and Sort Dropdowns */}
+                <div className="md:col-span-3 flex space-x-4">
+                    <input 
+                        type="text" 
+                        placeholder="Search by product model..." 
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gradient-to-r  from-indigo-800 to-fuchsia-500 text-white"
+                    />
+                    <button 
+                        onClick={handleSearchClick}
+                        className="px-4 py-2 bg-gradient-to-r  from-indigo-800 to-fuchsia-500 rounded-lg"
+                    >
+                        Search
+                    </button>
+                </div>
+
                 <select 
                     name="brand" 
                     onChange={handleFilterChange} 
-                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 z-50"
+                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gradient-to-r  from-indigo-800 to-fuchsia-500"
                 >
                     <option value="">All Brands</option>
                     <option value="Apple">Apple</option>
@@ -99,54 +99,57 @@ const ProductList = () => {
                     <option value="Microsoft">Microsoft</option>
                     <option value="Razer">Razer</option>
                     <option value="Sony">Sony</option>
-                    {/* Other options */}
                 </select>
 
                 <select 
                     name="category" 
                     onChange={handleFilterChange} 
-                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gradient-to-r  from-indigo-800 to-fuchsia-500"
                 >
                     <option value="">All Categories</option>
                     <option value="Headphone">Headphone</option>
                     <option value="Keyboard">Keyboard</option>
                     <option value="Mouse">Mouse</option>
-                    {/* Other options */}
                 </select>
 
                 <select 
                     name="priceRange" 
                     onChange={handleFilterChange} 
-                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gradient-to-r  from-indigo-800 to-fuchsia-500"
                 >
                     <option value="">All Prices</option>
-                    {/* Other options */}
+                    <option value="0-50">$0 - $50</option>
+                    <option value="51-60">$51 - $60</option>
+                    <option value="61-70">$61 - $70</option>
+                    <option value="71-100">$71 - $100</option>
+                    <option value="101-1000">$101 - $1000</option>
                 </select>
 
                 <select 
                     onChange={handleSortChange} 
-                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-gradient-to-r  from-indigo-800 to-fuchsia-500"
                 >
                     <option value="">Sort by</option>
-                    {/* Other options */}
+                    <option value="priceLowToHigh">Price: Low to High</option>
+                    <option value="priceHighToLow">Price: High to Low</option>
+                    <option value="newest">Date Added: Newest First</option>
                 </select>
             </div>
 
-            {/* Products Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 {products.map(product => (
                     <div 
                         key={product._id} 
-                        className="border p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300"
+                        className="border p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-300 bg-gradient-to-b  from-white "
                     >
                         <img src={product.image} alt={product.model} className="w-full max-h-80 mb-4 rounded-lg" />
-                        <h3 className="text-xl font-semibold">{product.model}</h3>
-                        <p className="text-gray-600">{product.category} by {product.brand}</p>
+                        <h3 className="text-xl font-semibold text-gray-800">{product.model}</h3>
+                        <p className="text-gray-700">{product.category} by {product.brand}</p>
                         <p className="text-gray-800 font-bold">${product.price}</p>
-                        <p className="text-sm text-gray-500">{new Date(product.date).toLocaleDateString()}</p>
+                        <p className="text-sm text-gray-700">{new Date(product.date).toLocaleDateString()}</p>
                         <button 
                             onClick={() => handleProductClick(product)} 
-                            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                            className="mt-4 px-4 py-2 bg-gradient-to-r  from-indigo-800 to-fuchsia-500 rounded-lg"
                         >
                             Details
                         </button>
@@ -154,12 +157,11 @@ const ProductList = () => {
                 ))}
             </div>
 
-            {/* Pagination */}
-            <div className="flex justify-center mt-10 space-x-2">
+            <div className="flex justify-center mt-10 flex-wrap gap-2">
                 <button 
                     onClick={() => setPage(page - 1)} 
                     disabled={page === 1}
-                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 disabled:bg-gray-300"
+                    className="px-4 py-2 bg-fuchsia-500 text-white rounded-lg hover:bg-indigo-800 disabled:bg-gray-300"
                 >
                     Previous
                 </button>
@@ -167,7 +169,7 @@ const ProductList = () => {
                     <button 
                         key={index + 1} 
                         onClick={() => setPage(index + 1)}
-                        className={`px-4 py-2 rounded-lg ${page === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                        className={`px-4 py-2 rounded-lg ${page === index + 1 ? 'bg-fuchsia-500 text-white' : 'bg-indigo-800'}`}
                     >
                         {index + 1}
                     </button>
@@ -175,26 +177,25 @@ const ProductList = () => {
                 <button 
                     onClick={() => setPage(page + 1)} 
                     disabled={page === totalPages}
-                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 disabled:bg-gray-300"
+                    className="px-4 py-2 bg-fuchsia-500 text-white rounded-lg hover:bg-indigo-800 disabled:bg-gray-300"
                 >
                     Next
                 </button>
             </div>
 
-            {/* Modal */}
             {selectedProduct && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                    <div className="bg-white p-6 rounded-lg max-w-lg w-full">
-                        <h2 className="text-2xl font-bold mb-4">{selectedProduct.model}</h2>
-                        <img src={selectedProduct.image} alt={selectedProduct.model} className="w-full object-cover mb-4 rounded-lg" />
-                        <p><strong>Brand:</strong> {selectedProduct.brand}</p>
-                        <p><strong>Category:</strong> {selectedProduct.category}</p>
-                        <p><strong>Description:</strong> {selectedProduct.description}</p>
-                        <p><strong>Price:</strong> ${selectedProduct.price}</p>
-                        <p><strong>Date Added:</strong> {new Date(selectedProduct.date).toLocaleDateString()}</p>
+                    <div className="p-6 rounded-lg max-w-lg w-full bg-gradient-to-bl  from-indigo-500 to-fuchsia-200">
+                        <h2 className="text-2xl font-bold mb-4 text-gray-800">{selectedProduct.model}</h2>
+                        <img src={selectedProduct.image} alt={selectedProduct.model} className="w-full max-h-80 mb-4 rounded-lg" />
+                        <p className="text-gray-800 mb-2">Brand: {selectedProduct.brand}</p>
+                        <p className="text-gray-800 mb-2">Category: {selectedProduct.category}</p>
+                        <p className="text-gray-800 font-bold mb-2">Price: ${selectedProduct.price}</p>
+                        <p className="text-sm text-gray-800 mb-4">Added on: {new Date(selectedProduct.date).toLocaleDateString()}</p>
+                        <p className="text-gray-900">{selectedProduct.description}</p>
                         <button 
                             onClick={handleCloseModal} 
-                            className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+                            className="mt-6 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
                         >
                             Close
                         </button>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,23 +4,24 @@ import useAuth from "../Hook/useAuth";
 const Home = () => {
   const { logout } = useAuth();
   return (
-    <div className="container mx-auto">
-      <div>
-        
-        <div className="flex justify-between items-center">
-        <h1 className="text-xl font-black">GADGET GALAXY</h1>
-          <button onClick={logout} className="btn btn-error text-white my-4">
-            Logout
-          </button>
+    <div className="bg-gradient-to-b  from-indigo-400 to-fuchsia-900">
+      <div className="container mx-auto">
+        <div>
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl font-black text-gray-800">GADGET GALAXY</h1>
+            <button onClick={logout} className="btn btn-error text-white my-4">
+              Logout
+            </button>
+          </div>
         </div>
-      </div>
-      <img
-        src="https://i.postimg.cc/BvLLX6Z0/banner1.jpg"
-        alt=""
-        className="my-5 rounded-2xl w-full"
-      />
-      <div>
-        <ProductList></ProductList>
+        <img
+          src="https://i.postimg.cc/BvLLX6Z0/banner1.jpg"
+          alt=""
+          className="my-5 rounded-2xl w-full"
+        />
+        <div>
+          <ProductList></ProductList>
+        </div>
       </div>
     </div>
   );
