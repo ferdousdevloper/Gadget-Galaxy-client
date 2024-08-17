31b09da24ee0d768447d6d0503c70ba145c229cf
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-08-17 16:13:03 +0600

    404 error page page implement

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,13 @@ import Home from "../Pages/Home";
 import Login from './../Pages/Login';
 import Register from './../Pages/Register';
 import PrivateRoute from "./PrivateRoute";
+import ErrorPage404 from "../Pages/ErrorPage404";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <PrivateRoute> <Home></Home></PrivateRoute>,
+    errorElement: <ErrorPage404></ErrorPage404>,
   },
   {
     path:"/login",
